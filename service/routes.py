de1db18974c5fb136268cc7444cf13c43ee5aeff
risqi <risqiikhsani16@gmail.com>
2024-12-09 20:47:50 -0500

    rest api

--- service/routes.py
+++ service/routes.py
@@ -15,8 +15,8 @@ from . import app  # Import Flask application
 ############################################################
 @app.route("/health")
 def health():
-    """Health Status"""
-    return jsonify(dict(status="OK")), status.HTTP_200_OK
+	"""Health Status"""
+	return jsonify(dict(status="OK")), status.HTTP_200_OK
 
 
 ######################################################################
@@ -24,15 +24,15 @@ def health():
 ######################################################################
 @app.route("/")
 def index():
-    """Root URL response"""
-    return (
-        jsonify(
-            name="Account REST API Service",
-            version="1.0",
-            # paths=url_for("list_accounts", _external=True),
-        ),
-        status.HTTP_200_OK,
-    )
+	"""Root URL response"""
+	return (
+		jsonify(
+			name="Account REST API Service",
+			version="1.0",
+			# paths=url_for("list_accounts", _external=True),
+		),
+		status.HTTP_200_OK,
+	)
 
 
 ######################################################################
@@ -40,57 +40,79 @@ def index():
 ######################################################################
 @app.route("/accounts", methods=["POST"])
 def create_accounts():
-    """
-    Creates an Account
-    This endpoint will create an Account based the data in the body that is posted
-    """
-    app.logger.info("Request to create an Account")
-    check_content_type("application/json")
-    account = Account()
-    account.deserialize(request.get_json())
-    account.create()
-    message = account.serialize()
-    # Uncomment once get_accounts has been implemented
-    # location_url = url_for("get_accounts", account_id=account.id, _external=True)
-    location_url = "/"  # Remove once get_accounts has been implemented
-    return make_response(
-        jsonify(message), status.HTTP_201_CREATED, {"Location": location_url}
-    )
+	"""
+	Creates an Account
+	This endpoint will create an Account based the data in the body that is posted
+	"""
+	app.logger.info("Request to create an Account")
+	check_content_type("application/json")
+	account = Account()
+	account.deserialize(request.get_json())
+	account.create()
+	message = account.serialize()
+	# Uncomment once get_accounts has been implemented
+	# location_url = url_for("get_accounts", account_id=account.id, _external=True)
+	location_url = "/"  # Remove once get_accounts has been implemented
+	return make_response(
+		jsonify(message), status.HTTP_201_CREATED, {"Location": location_url}
+	)
 
 ######################################################################
 # LIST ALL ACCOUNTS
 ######################################################################
 
 # ... place you code here to LIST accounts ...
-@app.route("/accounts/<int:account_id>",methods=["GET"])
-def get_accounts(account_id):
-    app.logger.info("Request to read an account with id: %s",account_id)
-    account = Account.find(account_id)
-    if not account:
-        abort(status.HTTP_404_NOT_FOUND,f"Account with id [{account_id}] could not be found.")
-
-    return account.serialize(),status.HTTP_200_OK
+@app.route("/accounts",methods=["GET"])
+def list_accounts():
+	app.logger.info("Request to list accounts")
+	accounts = Account.all()
+	account_list = [account.serialize() for account in accounts]
+	app.logger.info("Returning [%s] accounts",len(account_list))
+	return jsonify(account_list),status.HTTP_200_OK
 
 ######################################################################
 # READ AN ACCOUNT
 ######################################################################
 
 # ... place you code here to READ an account ...
+@app.route("/accounts/<int:account_id>",methods=["GET"])
+def get_accounts(account_id):
+	app.logger.info("Request to read an account with id: %s",account_id)
+	account = Account.find(account_id)
+	if not account:
+		abort(status.HTTP_404_NOT_FOUND,f"Account with id [{account_id}] could not be found.")
 
+	return account.serialize(),status.HTTP_200_OK
 
 ######################################################################
 # UPDATE AN EXISTING ACCOUNT
 ######################################################################
 
 # ... place you code here to UPDATE an account ...
+@app.route("/accounts/<int:account_id>",methods=["PUT"])
+def update_accounts(account_id):
+	app.logger.info("Request to update an acocunt with id: %s", account_id)
+	account = Account.find(account_id)
+	if not account:
+		abort(status.HTTP_404_NOT_FOUND, f"Account with id [{account_id}] could not be found.")
 
+	account.deserialize(request.get_json())
+	account.update()
+
+	return account.serialize(), status.HTTP_200_OK
 
 ######################################################################
 # DELETE AN ACCOUNT
 ######################################################################
 
 # ... place you code here to DELETE an account ...
-
+@app.route("/accounts/<int:account_id>",methods=["DELETE"])
+def delete_accounts(account_id):
+	app.logger.info("Request to delete an account with id: %s",account_id)
+	account = Account.find(account_id)
+	if account:
+		account.delete()
+	return "",status.HTTP_204_NO_CONTENT
 
 ######################################################################
 #  U T I L I T Y   F U N C T I O N S
@@ -98,12 +120,12 @@ def get_accounts(account_id):
 
 
 def check_content_type(media_type):
-    """Checks that the media type is correct"""
-    content_type = request.headers.get("Content-Type")
-    if content_type and content_type == media_type:
-        return
-    app.logger.error("Invalid Content-Type: %s", content_type)
-    abort(
-        status.HTTP_415_UNSUPPORTED_MEDIA_TYPE,
-        f"Content-Type must be {media_type}",
-    )
+	"""Checks that the media type is correct"""
+	content_type = request.headers.get("Content-Type")
+	if content_type and content_type == media_type:
+		return
+	app.logger.error("Invalid Content-Type: %s", content_type)
+	abort(
+		status.HTTP_415_UNSUPPORTED_MEDIA_TYPE,
+		f"Content-Type must be {media_type}",
+	)
